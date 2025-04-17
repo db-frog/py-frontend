0b88a3e8bc539b5df79b5c292642893f3fcea0e8
Daniel Benett <dannybenett@gmail.com>
2025-04-17 13:07:04 -0700

    update text

--- src/views/PreAuth.vue
+++ src/views/PreAuth.vue
@@ -2,27 +2,25 @@
   <Header />
   <div class="page-container">
     <p>
-      Berkeley Folklore Archives: a storehouse for myths and ballads, legends, tongue-twisters, riddles, toasts, blessings,
-      curses and all other material passed by word of mouth from generation to generation--are being established on this digital portal.
+      The Berkeley Folklore Archive: a storehouse for myths and ballads, legends, tongue-twisters, riddles, toasts, blessings,
+      curses and all other material passed by word of mouth from generation to generation.
     </p>
     <p>
-      The project snowballs through collection by folklore students of data from friends and relatives and personal recollections.
-      When Professor Alan Dundes began the project in 1963, he gathered items into file cabinets in his UC Berkeley office.
-      Over sixty years later, the physical archives consists of nearly 1 million items crammed into a windowless room in the heart of the Anthropology and Art Practices Building.
-      Overflow items are collected in the basement beneath the Hearst swimming pool –– a moist location not well-suited to archival preservation.
-      Approximately 1,000 new items are added each year. Thus, digitization.
+      Professor Alan Dundes began the project in 1963, when he gathered student-submitted items into file cabinets in his UC Berkeley office.
+      Over sixty years later, the physical archive consists of nearly 1 million items crammed into a windowless room in the heart of the Anthropology and Art Practices Building.
+      Approximately 1,000 new items are added each year, and storage is overflowing available space.
     </p>
     <p>
-      The archive, as Alan Dundes recognized, provides raw material for research in behavioral sciences as well as the field of folklore.
-      They can be an aid for authors, artists, political scientists, data scientists, sociologists, and psychologists.
+      And so, digitization.
     </p>
     <p>
       If you'd like to visit the physical archive, please make an appointment by emailing <a href="mailto:FOLKLORE_ARCHIVE@BERKELEY.EDU"> the archive </a>.
     </p>
-    <p>This text paraphrases and references the original "Berkeley folklore archives press release" accessed via the
+    <p>This text paraphrases and references the original, 1965 "Berkeley folklore archives press release" accessed via the
       Charles Deering McCormick Library of Special Collections at Northwestern University Libraries:
       <a href="https://dc.library.northwestern.edu/items/919b9ab6-f074-46fc-b073-47666a4f8d55"> https://dc.library.northwestern.edu/items/919b9ab6-f074-46fc-b073-47666a4f8d55 </a>
     </p>
+    <img src="@/assets/dundesinarchive.jpg" alt="Professor Alan Dundes in the Berkeley Folklore Archive" class="dundes">
   </div>
 
 </template>
@@ -46,5 +44,9 @@ export default defineComponent({
   background-color: var(--color-primary-white);
   max-height: 100vh;
   padding: 1rem;
+}
+.dundes {
+  max-width: 300px;
+  margin: auto;
 }
 </style>
